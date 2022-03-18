c37e58c1183205a590c7d285c540370a71849e8f
Sonarie <sonarie1980@gmail.com>
2022-03-18 18:59:29 -0500

    code done

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,40 +1,44 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
-// GET /api/users
-router.get("/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "post_url", "title", "created_at"],
-    include: [
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
+// get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
   })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      res.json(dbPostData);
-    })
+    .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-// GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
     },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        through: Vote,
+        as: "voted_posts",
+      },
+    ],
   })
     .then((dbUserData) => {
       if (!dbUserData) {
@@ -49,15 +53,14 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// POST /api/users
 router.post("/", (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.body.user_id,
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
   })
-    .then((dbPostData) => res.json(dbPostData))
+    .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -77,6 +80,7 @@ router.post("/login", (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: "Incorrect password!" });
       return;
@@ -86,24 +90,22 @@ router.post("/login", (req, res) => {
   });
 });
 
-// PUT /api/users/1
 router.put("/:id", (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
     },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
+  })
+    .then((dbUserData) => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
-      res.json(dbPostData);
+      res.json(dbUserData);
     })
     .catch((err) => {
       console.log(err);
@@ -111,19 +113,18 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 router.delete("/:id", (req, res) => {
-  Post.destroy({
+  User.destroy({
     where: {
       id: req.params.id,
     },
   })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
-      res.json(dbPostData);
+      res.json(dbUserData);
     })
     .catch((err) => {
       console.log(err);
