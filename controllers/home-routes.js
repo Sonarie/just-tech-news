75884a04292cb9f9c27880ea9bc314aa2bb253a7
Sonarie <sonarie1980@gmail.com>
2022-03-22 16:17:05 -0500

    lesson file compare fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Vote } = require("../models");
 
+// get all posts for homepage
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
     attributes: [
       "id",
@@ -30,8 +34,8 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
       res.render("homepage", { posts });
     })
     .catch((err) => {
@@ -48,8 +52,5 @@ router.get("/login", (req, res) => {
 
   res.render("login");
 });
-
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
 
 module.exports = router;
